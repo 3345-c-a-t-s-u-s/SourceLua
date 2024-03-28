e52aff0a97a7a68a72cca43dd95ed8e12c707e3f
CAT_SUS <117000269+3345-c-a-t-s-u-s@users.noreply.github.com>
2024-03-28 14:34:33 +0700

    Update Kaiju_Paradise.lua

--- Kaiju_Paradise.lua
+++ Kaiju_Paradise.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/3345-c-a-t-s-u-s/SourceLua/main/Kaiju_ParadiseNewUI.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/3345-c-a-t-s-u-s/New-C4-Remote.lua/main/KaijuParadise.cs"))()
